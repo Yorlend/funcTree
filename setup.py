9b5962096277bec3ca0fbee0038660cc4c14c696
yorlend <yorlend@github.com>
2021-06-22 16:08:42 +0300

    lib structure initialized

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cfunctree',
+    packages=find_packages(include=['cfunctree']),
+    version='0.1.0',
+    description='Graphical visualization of function calls in c programmes',
+    author='Yorlend, Rull Deef',
+    license='MIT',
+    install_requires=[
+        'pycparser==2.20',
+        'graphviz==0.16',
+        
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==6.2.4'],
+    test_suite='tests',
+)
